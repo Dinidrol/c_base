11d1694906f2128cb4ea148b015faadca082c312
Dinidrol <vamovitch@gmail.com>
2019-07-01 00:13:33 +0300

    Add files via upload
    Работа над ошибками. 
    Добавил проверку вводимого числа.
    Убрал копипаст.
    Поправил функцию вычисления div2.

--- myfile.cpp
+++ myfile.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include <bitset>
+#include <limits>
  
 using std::cout;
 using std::cin;
@@ -18,18 +18,35 @@ enum {
 
 };
 
+int incriment_number (int best_div) {
+	int summ = 0;
+	while (best_div > 0) {
+		summ += best_div % 10;
+		best_div /= 10;
+	}
+	return summ;
+}
+
 void tree(){
-    int width = 0;
+    unsigned width_tree = 0;
+
     cout << "Input size tree:\n";
-    cin >> width;
-    if(width %2 == 0){
-        ++width;
+    
+    
+    while(!(cin >> width_tree)){
+        (cout << "Error! Re-Input size tree (NUMBER UNSIGNEd): ").flush();
+        cin.clear();
+        cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
     }
-    for(int i = width/2; i>=0; --i){
-        for (int j = 0; j < i; ++j) {
+
+    if(width_tree %2 == 0){
+        ++width_tree;
+    }
+    for(int iteration = width_tree/2; iteration>=0; --iteration){
+        for (int space = 0; space < iteration; ++space) {
 			cout<<' ';
 		}
-		for (int k = 1; k <= width - i * 2; ++k) {
+		for (int star = 1; star <= width_tree - iteration * 2; ++star) {
 			cout<<'*';
 		}
 	    cout<<"\n";
@@ -38,103 +55,132 @@ void tree(){
 
 
 void average (){
-    int summ = 0;
-    int counter = 0;
-    int input_number = 0;
+    unsigned summ = 0;
+    unsigned counter = 0;
+    unsigned input_number = 0;
     
 
-    cout << "Input number: ";
-    cin >> input_number;
+    cout << "program for calculating the average. Input only number: ";
+   
 
-    while(input_number > 0){
-        summ += input_number % 10;
-        input_number /= 10;
-        ++ counter;
+    while(!(cin >> input_number)){
+        (cout << "Error! Re-Input number (NUMBER UNSIGNED): ").flush();
+        cin.clear();
+        cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
     }
 
-    auto average = summ / counter;
-    cout << "Average: " << average << "\n";
+
+    if (input_number !=0){
+        while(input_number > 0){
+            summ += input_number % 10;
+            input_number /= 10;
+            ++ counter;
+        }
+
+        auto average = summ / counter;
+        cout << "Average: " << average << "\n";
+    } else {
+        cout << "Нou have not entered a single number \n";
+    }
+
 }
 
 void bits_summ(){
 
-    unsigned number = 0;
+    unsigned dec_number = 0;
     unsigned res = 0;
 
     cout <<"Input number to count set bit: ";
-    cin >> number;
-
-    for(int i=0; i<=31; i++) {
-        res += ((number & (1 << i)) != 0);
-    }
-    cout << res << "\n";    
     
-    printf("\n");
-    //Для проверки выражения
-    std::bitset<8> b(number);
-    printf("Check using bitset class %s\n", b.to_string().c_str());
+    
+    while(!(cin >> dec_number)){
+        (cout << "Error! Re-Input number (NUMBER UNSIGNED): ").flush();
+        cin.clear();
+        cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
+    }
+     
+
+    while (dec_number !=0){
+        res += dec_number & 1;
+        dec_number >>= 1;
+    }
+
+    cout <<"The amount of bits set in the number:"<< res << "\n";    
+    
 }
 
 void happy_ticket(){
-    int a, tmp1=0, tmp2=0;
+    unsigned ticket_number;
+    unsigned high_bit;
+    unsigned low_bit;
+
 
     cout <<"Input ticket number:\n";
-    cin>>a;
-
-    for(int i=0; i<3; i++){
-        tmp1+=a%10; a/=10;
+    
+    while(!(cin >> ticket_number)){
+        (cout << "Error! Re-Input number (NUMBER UNSIGNED): ").flush();
+        cin.clear();
+        cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
     }
 
-    for(int i=0; i<3; i++){
-        tmp2+=a%10; a/=10;
+    for(size_t i=0; i<3; i++){
+        high_bit+=ticket_number%10; 
+        ticket_number/=10;
     }
 
-    if(tmp1==tmp2){
-        cout<<"Yes\n";
-    } else {
-        cout<<"No\n";
+    for(size_t i=0; i<3; i++){
+        low_bit+=ticket_number%10; 
+        ticket_number/=10;
     }
+
+    (high_bit == low_bit)?(cout<<"You have are happy ticket!\n"):(cout<<"You have are  not happy ticket bro!\n");
+
 }
 
-void coup_number(){
+void revers_number(){
 
-    unsigned int n = 0,m = 0;
+    unsigned input_number = 0;
+    unsigned revers_number = 0;
 
     cout <<"Insert number:\n";
-    cin >> n;
     
-    while (n > 0) {
-        m = m*10 + n%10;
-        n = n/10;
+    while(!(cin >> input_number)){
+        (cout << "Error! Re-Input number (NUMBER UNSIGNED): ").flush();
+        cin.clear();
+        cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
+    }   
+    
+
+    while (input_number > 0) {
+        revers_number = revers_number*10 + input_number%10;
+        input_number = input_number/10;
     }
-    cout << m;
+    cout <<"You revers string number: "<< revers_number;
     
 }
 
 void odd_summ(){
-    constexpr int N = 4;
+    constexpr int N = 101;
     int mas[N]; 
     int sum=0;
 
-    cout <<"Insert number: \n";
+    cout <<"Insert 100 any integers number: \n";
 
-    for (int i = 0; i < N; i++){
+    for (size_t i = 0; i < N; i++){
         cin >> mas[i];  
-    }
-    
-    for (int i = 0; i < N; i++){
         if (mas[i]%2 != 0){
-            sum=sum+mas[i];
+            sum+=mas[i];
         }
     }
-    cout <<"Summ of elements: "<< sum << "\n";
+    
+    cout <<"Summ even of elements: "<< sum << "\n";
 }
 
 void bit_set(){
     unsigned number;
     unsigned bitNum;
 
-    cout << "Input number: \n";
+    cout << "Input number to check bit: \n";
     cin >> number;
 
     cout <<"Input bit set number: \n";
@@ -142,30 +188,35 @@ void bit_set(){
 
     number >>= bitNum-1;
 
-    if(number & 1){
-        cout << "Bit set";
+    if((number > 0 ) && (number <=32)  && (number &(0x1 << bitNum-1))){
+        cout << "Bit set in number";
     } else {
-        cout <<"Bit dont set";
+        cout <<"Bit dont set in number";
     }
 
 }
 
 void best_div(){
-    int number;
-    int div;
+    int number = 0;
+    int div = 1;
+    int previous_summ = 0;
+    int best_div = 0;
 
     cout << "Input number: \n";
     cin >> number;
 
-    int half = number/2;
-
-    for(int i = 1; i <= half; ++i ){
-        if(number % i == 0){
-            div = i;
+    while(div <= number){
+        int current_summ = 0;
+        if(!(number % div)){
+            current_summ = incriment_number(div);
+            if (current_summ >= previous_summ){
+                best_div = div;
+                previous_summ = current_summ;
+            }
         }
+        ++div;
     }
-
-    cout << "Best div:" << div;
+    cout << "Best div:" << best_div;
 }
 
 void div_by3(){
@@ -199,7 +250,7 @@ int main(){
     cout << "Calculation program: \n"
          <<"1. Build tree \n" 
          <<"2. Average \n" 
-         <<"3. Summ bit in number \n"
+         <<"3. Summ of bits in the number \n"
          <<"4. Happy ticket \n"
          <<"5. Inverse numbers \n"
          <<"6. The sum of odd elements\n"
@@ -223,7 +274,7 @@ int main(){
             happy_ticket();
             break;
         case REVERS_NUMBER:
-            coup_number();
+            revers_number();
             break;
         case ODD_SUMM:
             odd_summ();
